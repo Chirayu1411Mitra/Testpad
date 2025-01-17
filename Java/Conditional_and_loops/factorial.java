9ce72fc3c662ccce06799c21303be8fcb53a8b30
Chirayu Mitra <155830642+Chirayu1411Mitra@users.noreply.github.com>
2025-01-17 09:37:19 +0530

    Update factorial.java

--- Java/Conditional_and_loops/factorial.java
+++ Java/Conditional_and_loops/factorial.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-public class factorial{
 	static long fact(int n){
 	long fact=1;
 	 for(int i =1;i<=n;i++){
        	 fact=fact*i;
 	  }
 	    return fact;
-
-}
-
-	public static void main(String[] args){
-	Scanner input = new Scanner(System.in);
-	int n=input.nextInt();
-	long fact=fact(n);
-	System.out.println(fact);
-	}
 
 }
