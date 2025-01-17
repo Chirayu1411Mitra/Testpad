6cb37bc7f24c51c3000c0e5ad083fdb5d740743c
Chirayu Mitra <155830642+Chirayu1411Mitra@users.noreply.github.com>
2025-01-17 09:36:00 +0530

    Update sum_of_first_n_terms.java

--- Java/Conditional_and_loops/sum_of_first_n_terms.java
+++ Java/Conditional_and_loops/sum_of_first_n_terms.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;
-public class sum_of_first_n_terms{
-	public static void main(String[] args){
 	Scanner input = new Scanner (System.in);
         int sum=0;
         int n=input.nextInt();
@@ -8,6 +5,5 @@ public class sum_of_first_n_terms{
             sum=sum+i;
         }
         System.out.println(sum);
-    }
-}
+    
 
