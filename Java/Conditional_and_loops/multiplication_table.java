a6242c2d4ea73559a94bec8138e403f50d5a85d4
Chirayu Mitra <155830642+Chirayu1411Mitra@users.noreply.github.com>
2025-01-17 09:36:35 +0530

    Update multiplication_table.java

--- Java/Conditional_and_loops/multiplication_table.java
+++ Java/Conditional_and_loops/multiplication_table.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-public class multiplication_table{
-	public static void main(String[] args){
 	 Scanner input = new Scanner(System.in);
       	 int n = input.nextInt();
      	 int m= input.nextInt();
        	  for (int i=1;i<=m;i++){
          	 System.out.println(n*i);
       		}
- 	}
-}
+	
