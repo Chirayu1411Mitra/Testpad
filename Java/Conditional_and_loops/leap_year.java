342aa5685b4aed8aabcf78d1fd148d383dc0942d
Chirayu Mitra <155830642+Chirayu1411Mitra@users.noreply.github.com>
2025-01-17 09:34:54 +0530

    Update leap_year.java

--- Java/Conditional_and_loops/leap_year.java
+++ Java/Conditional_and_loops/leap_year.java
@@ -1,15 +1,9 @@
-import java.util.Scanner;
-public class leap_year{
-	public static void main(String [] args){
-		 Scanner input=new Scanner(System.in);
-     		 int year;
-     		 year=input.nextInt();
-      		if((year%4==0 && year%100!=0)||(year%400==0)){
-         		 System.out.println("Leap Year");
-     		 }
-     		   else{
-        		 System.out.println("Not a Leap Year");
- 	  	 }
-
-	}
+Scanner input=new Scanner(System.in);
+int year;
+year=input.nextInt();
+if((year%4==0 && year%100!=0)||(year%400==0)){
+	System.out.println("Leap Year");
+}
+else{
+	System.out.println("Not a Leap Year");
 }
